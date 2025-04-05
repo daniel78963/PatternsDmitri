7ac86c7f1b5e1ec3166a668bec82a4b2327a81af
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-04-05 10:50:37 -0500

    add Rectangle

---
+++ Dmitri/ConsolePatternsNETFramework/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsolePatternsNETFramework
+{
+    public class Rectangle
+    {
+        public Rectangle(int width, int heigth)
+        {
+            Width = width;
+            Heigth = heigth;
+        }
+
+        public int Width { get; }
+        public int Heigth { get; }
+    }
+}
